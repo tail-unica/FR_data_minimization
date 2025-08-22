02266df8ce84b1aee69ec22c45fc45b63a1282f9
atzoriandrea <atzoriandrea@outlook.it>
2025-08-22 13:14:32 +0200

    Added new results, FIQ quality scores (CRFIQA, GRAFIQs) for WF, GC, DC, IDF

--- batch_train_resnet.sh
+++ batch_train_resnet.sh
@@ -68,7 +68,7 @@ for synth_ds in "GC" "DC" "IDF"; do
   for synth_id in $(seq 2000 2000 8000); do
       for cmt in "descending" "ascending"; do
       # Execute the train.py script with the current combination of parameters
-      CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --synth_ds $synth_ds --synth_id $synth_id --auth_id 0 --experiment "quality_crfiqa_std" --cmt $cmt
+      CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --synth_ds $synth_ds --synth_id $synth_id --auth_id 0 --experiment "quality_grafiqs_std" --cmt $cmt
       ps -ef | grep "train" | grep -v grep | awk '{print "kill -9 "$2}' | sh
       done
   done
@@ -78,7 +78,7 @@ for auth_ds in "WF"; do
     for auth_id in $(seq 2000 2000 8000); do
         for cmt in "descending" "ascending"; do
           # Execute the train.py script with the current combination of parameters
-          CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --auth_ds $auth_ds --synth_id 0 --auth_id $auth_id --experiment "quality_crfiqa_std" --cmt $cmt
+          CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --auth_ds $auth_ds --synth_id 0 --auth_id $auth_id --experiment "quality_grafiqs_std" --cmt $cmt
           ps -ef | grep "train" | grep -v grep | awk '{print "kill -9 "$2}' | sh
         done
     done
@@ -88,7 +88,7 @@ for synth_ds in "GC" "DC" "IDF"; do
   for synth_id in $(seq 2000 2000 8000); do
       for cmt in "descending" "ascending"; do
       # Execute the train.py script with the current combination of parameters
-      CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --synth_ds $synth_ds --synth_id $synth_id --auth_id 0 --experiment "quality_crfiqa_mean" --cmt $cmt
+      CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --synth_ds $synth_ds --synth_id $synth_id --auth_id 0 --experiment "quality_grafiqs_mean" --cmt $cmt
       ps -ef | grep "train" | grep -v grep | awk '{print "kill -9 "$2}' | sh
       done
   done
@@ -98,7 +98,7 @@ for auth_ds in "M2-S" "WF"; do
     for auth_id in $(seq 2000 2000 8000); do
         for cmt in "descending" "ascending"; do
           # Execute the train.py script with the current combination of parameters
-          CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --auth_ds $auth_ds --synth_id 0 --auth_id $auth_id --experiment "quality_crfiqa_mean" --cmt $cmt
+          CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1236 train.py --auth_ds $auth_ds --synth_id 0 --auth_id $auth_id --experiment "quality_grafiqs_mean" --cmt $cmt
           ps -ef | grep "train" | grep -v grep | awk '{print "kill -9 "$2}' | sh
         done
     done
